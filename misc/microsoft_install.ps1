91ebbe412bdf7daced2fa31dde1ab71144db5e28
Joshua Person <noonker@gmail.com>
2017-11-21 13:55:48 -0500

    Winter Cleaning

--- misc/microsoft_install.ps1
+++ misc/microsoft_install.ps1
@@ -1,23 +1,19 @@
 $programs=
   ("7zip", "http://www.7-zip.org/download.html"),
   ("Ableton", "https://www.ableton.com/en/account/"),
-  ("Affinity", "https://affinity.store/en-us/sign-in/"),
-  ("Audacity", "http://old.audacityteam.org/download/"),
-  ("Battle.net", "https://us.battle.net/download/en/"),
-  ("Roland Drivers", "https://www.roland.com/global/support/by_product/duo-capture_ex/updates_drivers/"),
   ("GnuRadio", "http://www.gcndevelopment.com/gnuradio/downloads.htm"),
   ("QT", "https://www.qt.io/download/"),
-  ("Chrome", "https://www.qt.io/download/"),
   ("Native Instruments", "https://www.native-instruments.com/en/login/?redirect=%2Fen%2Fmy-account%2F"),
   ("qBittorrent", "http://www.qbittorrent.org/download.php"),
   ("Keith McMillen", "https://www.keithmcmillen.com/downloads/"),
-  ("SharkpKeys", "http://sharpkeys.codeplex.com/"),
-  ("Steam", "http://store.steampowered.com/about/"),
-  ("Synergy", "https://symless.com/account/"),
   ("VLC", "http://www.videolan.org/vlc/index.html"),
   ("Wireshark", "https://www.wireshark.org/#download"),
   ("xming", "https://sourceforge.net/projects/xming/"),
-  ("Anaconda", "https://www.continuum.io/downloads")
+  ("Anaconda", "https://www.continuum.io/downloads"),
+  ("Firefox", "https://www.mozilla.org/en-US/firefox/developer/"),
+  ("Emacs", "https://www.gnu.org/software/emacs/"),
+  ("Msys2", "http://www.msys2.org/"),
+  ("Kindle", "https://www.amazon.com/kindlepcdownload/")
 
 ForEach ($item in $programs) {
   Start-Process $item[1]
--- misc/westminster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-E=329.63
-Gs=415.30
-Fs=369.99
-B=246.94
-for X in $E $Gs $Fs $B $E $Fs $Gs $E $Gs $E $Fs $B $B $Fs $Gs $E
-do
-speaker-test -c1 -f$X -t sine -l1 -b5 > /dev/null
-done
