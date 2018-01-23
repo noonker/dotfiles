4682421a3071e97efdc64e06cfb9a7911e24c8ac
Joshua Person <noonker@gmail.com>
2018-01-22 23:21:04 -0500

    Some windows changes. Will need more work later

--- misc/microsoft_install.ps1
+++ misc/microsoft_install.ps1
@@ -23,3 +23,23 @@ ForEach ($item in $programs) {
   Start-Process $item[1]
 }
 
+
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+cinst Gow
+cinst git.install
+cinst poshgit
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+cinst Git-Credential-Manager-for-Windows
+cinst github
+cinst python
+cinst pip
+cinst easy.install
+cinst docker-for-windows
+cinst emacs
+setx HOME %USERPROFILE%
+setx PATH "%PATH%;%USERPROFILE%\bin"
+cinst Gpg4win
+cinst ag
+cinst miktex 
